7ce1a2c2cabe92c7fc32e874232d2c728b1c20a5
davidchencsl <davidchencsl@gmail.com>
2022-08-01 01:46:28 -0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use std::fs;
+use std::io;
+use std::env::args;
+
+fn main() {
+    let mut machine = TapeMachine {
+        tape: vec![0; 30000],
+        data_ptr: 0,
+    };
+    let filename = args().nth(1).expect("No filename provided");
+    execute(&parse_file(&filename), &mut machine);
+    debug_print(&machine);
+}
+
+struct TapeMachine{
+    tape: Vec<u8>,
+    data_ptr: usize,
+}
+
+fn parse_file(filename: &str) -> String {
+    let contents = fs::read_to_string(filename).expect("Unable to read file");
+    return contents;
+}
+
+fn debug_print(machine: &TapeMachine) {
+    log::debug!("{:?}", &machine.tape[0..10]);
+}
+
+fn execute(cmd: &str, machine: &mut TapeMachine) {
+    let mut i = 0;
+    while i < cmd.len() {
+        let c = cmd.chars().nth(i).unwrap();
+        match c {
+            '+' => {
+                machine.tape[machine.data_ptr] += 1;
+            }
+            '-' => {
+                machine.tape[machine.data_ptr] -= 1;
+            }
+            '>' => {
+                machine.data_ptr += 1;
+            }
+            '<' => {
+                if machine.data_ptr == 0 {
+                    panic!("Moving pointer to negative index!");
+                }
+                machine.data_ptr -= 1;
+            }
+            '.' => {
+                print!("{}", &(machine.tape[machine.data_ptr] as char));
+            }
+            ',' => {
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).unwrap();
+                machine.tape[machine.data_ptr] = match input.trim().chars().nth(0) {
+                    Some(c) => c as u8,
+                    None => 10,
+                    };
+            }
+            '[' => {
+                if machine.tape[machine.data_ptr] == 0 {
+                    let mut bracket_count = 0;
+                    for j in i+1..cmd.len() {
+                        let character = cmd.chars().nth(j).unwrap();
+                        if character == '[' {
+                            bracket_count += 1;
+                        }
+                        if character == ']' && bracket_count == 0 {
+                            i = j;
+                            break;
+                        }
+                        if character == ']' {
+                            bracket_count -= 1;
+                        }
+                    }
+                    if bracket_count != 0 {
+                        panic!("Unmatched brackets [");
+                    }
+                }
+            }
+            ']' => {
+                if machine.tape[machine.data_ptr] != 0 {
+                    let mut bracket_count = 0;
+                    for j in (0..i).rev() {
+                        let character = cmd.chars().nth(j).unwrap();
+                        if character == ']' {
+                            bracket_count += 1;
+                        }
+                        if character == '[' && bracket_count == 0 {
+                            i = j;
+                            break;
+                        }
+                        if character == '[' {
+                            bracket_count -= 1;
+                        }
+                    }
+                    if bracket_count != 0 {
+                        panic!("Unmatched brackets ]");
+                    }
+                }
+            }
+            _ => {
+            }
+        }
+        i += 1;
+    }
+}
